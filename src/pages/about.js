dc7b7b35aebe1fc3d03f706d2fa327966afb8e36
Sam Soffes <sam@soff.es>
2020-01-28 04:44:17 +0800

    Tutorial part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,31 @@
 import React from "react"
 
+import Container from "../components/container"
+import styles from "./about.module.css"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Container>
     <h1>About Gatsby</h1>
     <p>Such wow. Very React.</p>
-  </div>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Container from "../components/container"
+
 export default () => (
-  <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
     <Link to="/about">About</Link>
-  </div>
+  </Container>
 )
